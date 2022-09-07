b4aee81f5035bbe7c07b3ea0507191117e88ee3d
swekiiz <best_n.dol@hotmail.com>
2022-09-08 01:32:28 +0700

    setup

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,53 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { CacheProvider, EmotionCache, ThemeProvider as EmotionThemeProvider } from '@emotion/react'
+import { CssBaseline, ThemeProvider as MaterialThemeProvider, StyledEngineProvider } from '@mui/material'
+import { AppProps } from 'next/app'
+import Head from 'next/head'
+import { PageWrapper } from 'views/components/PageWrapper'
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+import { createEmotionCache } from 'libs/emotion'
+
+import { theme } from 'themes/main'
+
+interface CustomAppProps extends AppProps {
+  emotionCache?: EmotionCache
 }
 
-export default MyApp
+// * Client-side cache, shared for the whole session of the user in the browser.
+const clientSideEmotionCache = createEmotionCache()
+
+const CustomApp = ({ Component, emotionCache = clientSideEmotionCache, pageProps }: CustomAppProps) => {
+  return (
+    <>
+      <Head>
+        <title>CEPP</title>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+      <StyledEngineProvider injectFirst>
+        <CacheProvider value={emotionCache}>
+          <MaterialThemeProvider theme={theme}>
+            <EmotionThemeProvider theme={theme}>
+              <CssBaseline />
+              <PageWrapper>
+                <Component {...pageProps} />
+              </PageWrapper>
+            </EmotionThemeProvider>
+          </MaterialThemeProvider>
+        </CacheProvider>
+      </StyledEngineProvider>
+    </>
+  )
+}
+
+// Only uncomment this method if you have blocking data requirements for
+// every single page in your application. This disables the ability to
+// perform automatic static optimization, causing every page in your app to
+// be server-side rendered.
+//
+// CustomApp.getInitialProps = async (appContext: AppContext) => {
+//   // calls page's `getInitialProps` and fills `appProps.pageProps`
+//   const appProps = await App.getInitialProps(appContext);
+//
+//   return { ...appProps }
+// }
+
+export default CustomApp
